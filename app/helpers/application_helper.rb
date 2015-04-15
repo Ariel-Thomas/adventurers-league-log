34558236e46a8608bbda56e8e961b908c503db88
Ariel Thomas <Ariel.Thomas@gmail.com>
2015-04-14 23:49:27 -0500

    Fix character show page responsiveness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def display_attribute attribute_name, attribute_value
     ("<div class='row'>
-        <div class='col-md-4'>
+        <div class='col-sm-4'>
           <strong>" + attribute_name + ":</strong>
         </div>
-        <div class='col-md-8'>" +
+        <div class='col-sm-8'>" +
           attribute_value.to_s +
         "</div>
       </div>").html_safe
