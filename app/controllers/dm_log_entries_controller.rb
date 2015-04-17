ab8c64a1214e871a0419c06c6235f55f55b1773f
Ariel Thomas <Ariel.Thomas@gmail.com>
2015-04-17 05:16:41 -0500

    Allow guest users to see dm logs

--- app/controllers/dm_log_entries_controller.rb
+++ app/controllers/dm_log_entries_controller.rb
@@ -1,5 +1,5 @@
 class DmLogEntriesController < AuthenticationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   add_crumb('Home', '/')
   before_filter :load_user
