c6d6da506144dd1b20483e1124ac487c78f1c865
Ariel Thomas <Ariel.Thomas@gmail.com>
2015-04-19 22:38:17 -0500

    Add public users and characters to stats page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,13 @@ class HomeController < ApplicationController
   end
 
   def stats
-    @users_count       = User.count
-    @characters_count  = Character.count
-    @log_entries_count = LogEntry.count
+    @users_count             = User.count
+    @public_users_count      = User.where(publicly_visible: true).count
+    @characters_count        = Character.count
+    @public_characters_count = Character.where(publicly_visible: true).count
+    @log_entries_count       = LogEntry.count
 
-    @factions_count    = Faction.all.map{ |faction| [faction.name, faction.characters.count] }
+    @factions_count          = Faction.all.map{ |faction| [faction.name, faction.characters.count] }
 
   end
 end
