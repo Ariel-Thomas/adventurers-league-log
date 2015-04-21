473d485efb8182165c3d3a23d5628c5800500bad
Ariel Thomas <Ariel.Thomas@gmail.com>
2015-04-20 19:11:49 -0500

    Add host address to environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,5 @@ AdventurersLeagueLog::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ AdventurersLeagueLog::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
