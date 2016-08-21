901d5a53321e5146e31afe06dfa6146f3872f4b5
Ariel Thomas <Ariel.Thomas@gmail.com>
2016-08-21 04:12:44 -0500

    Fix position num for adventures.

---
+++ db/seeds/20160821040950_fix_position_num_for_adventures.rb
@@ -0,0 +1,17 @@
+class FixPositionNumForAdventures < PhilColumns::Seed
+
+  envs :test, :development, :production
+  tags :default
+
+  def up
+    AdventureFormInput.order(:id).all.each_with_index do |form_input, index|
+      form_input.update!(position_num: (index + 1) * 10)
+    end
+
+    AdventureFormInput.find_by(name: "Dungeon Master Quest").update(position_num: 1)
+  end
+
+  def down
+  end
+
+end
