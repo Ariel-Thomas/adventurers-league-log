cda5b606617757b1d0fa5176266301a020e9248e
Ariel Thomas <Ariel.Thomas@gmail.com>
2016-08-21 03:01:44 -0500

    Switch to SendGrid from Mandrill.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,12 @@ AdventurersLeagueLog::Application.configure do
 
   ActionMailer::Base.smtp_settings = {
     :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
+    :address =>        'smtp.sendgrid.net',
+    :user_name =>      ENV['SENDGRID_USERNAME'],
+    :password =>       ENV['SENDGRID_PASSWORD'],
     :domain =>         'heroku.com',
-    :authentication => :plain
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
   ActionMailer::Base.delivery_method = :smtp
 
