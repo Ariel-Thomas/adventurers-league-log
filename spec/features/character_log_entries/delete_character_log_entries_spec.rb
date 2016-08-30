5c26a0ab21533727029e9eb46c1f5c5e7ddda10b
Ariel Thomas <Ariel.Thomas@gmail.com>
2016-08-30 04:49:51 -0500

    Add Character Log Entry specs.

---
+++ spec/features/character_log_entries/delete_character_log_entries_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature 'Character Log Entries', type: :feature do
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    login_as(@user, scope: :user)
+    @character = FactoryGirl.create(:character, user: @user)
+    @character_log_entry = FactoryGirl.create(:character_log_entry, user: @user)
+    @character_log_entry.characters = [@character]
+  end
+
+  scenario 'Delete a Character Log Entry' do
+    @character_log_entry_count = CharacterLogEntry.count
+    visit user_character_path(@user, @character)
+
+    click_link 'Delete'
+    # find_link('Delete').trigger('click') # hack to fix previous line
+
+    expect(CharacterLogEntry.count).to be(@character_log_entry_count - 1)
+
+    expect(page).to have_text("Successfully deleted " + @character_log_entry.adventure_title)
+
+    visit user_character_path(@user, @character)
+
+    expect(page).to_not have_text(@character_log_entry.date_played.strftime('%Y-%m-%d %H:%M'))
+    expect(page).to_not have_text(@character_log_entry.adventure_title)
+    expect(page).to_not have_text(@character_log_entry.session_num)
+    expect(page).to_not have_text(@character_log_entry.xp_gained)
+    expect(page).to_not have_text(@character_log_entry.gp_gained.floor)
+    expect(page).to_not have_text(@character_log_entry.downtime_gained)
+  end
+end
---
+++ spec/features/character_log_entries/show_character_log_entries_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature 'Character Log Entries', type: :feature do
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    login_as(@user, scope: :user)
+    @character    = FactoryGirl.create(:character, user: @user)
+    @character_log_entry = FactoryGirl.create(:character_log_entry, user: @user)
+    @character_log_entry.characters = [@character]
+  end
+
+  scenario 'Character Log Entry index page should have information' do
+    visit user_character_path(@user, @character)
+
+    expect(page).to have_text(@character_log_entry.date_played.strftime('%Y-%m-%d %H:%M'))
+    expect(page).to have_text(@character_log_entry.adventure_title)
+    expect(page).to have_text(@character_log_entry.session_num)
+    expect(page).to have_text(@character_log_entry.xp_gained)
+    expect(page).to have_text(@character_log_entry.gp_gained.floor)
+    expect(page).to have_text(@character_log_entry.downtime_gained)
+  end
+
+  scenario 'Character Log Entry show page should have information' do
+    visit user_character_character_log_entry_path(@user, @character, @character_log_entry)
+
+    expect(page).to have_text(@character_log_entry.adventure_title)
+    expect(page).to have_text(@character_log_entry.session_num)
+    expect(page).to have_text(@character_log_entry.date_played.strftime('%Y-%m-%d %H:%M'))
+    expect(page).to have_text(@character_log_entry.xp_gained)
+    expect(page).to have_text(@character_log_entry.gp_gained.floor)
+    expect(page).to have_text(@character_log_entry.downtime_gained)
+    expect(page).to have_text(@character_log_entry.location_played)
+    expect(page).to have_text(@character_log_entry.notes)
+  end
+end
