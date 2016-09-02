b0bb41f9cd6c6cf16adf15b93c49d79c024cc770
Ariel Thomas <Ariel.Thomas@gmail.com>
2016-09-02 01:03:38 -0500

    Fix bug where updating campaign log always selected every character.

--- app/controllers/campaign_log_entries_controller.rb
+++ app/controllers/campaign_log_entries_controller.rb
@@ -4,15 +4,27 @@ class CampaignLogEntriesController < AuthenticationController
   add_crumb('Home', '/')
   before_filter :load_user
   before_filter :load_character, only: [:show]
-  before_filter :load_campaign,  only: [:show, :new, :create, :edit, :update, :destroy]
-  before_filter :load_log_entry, only: [:show, :edit, :update, :destroy]
-  before_filter :load_adventure_form_inputs, only: [:new, :create, :edit, :update]
+  before_filter :load_campaign,
+                only: [:show, :new, :create, :edit, :update, :destroy]
+  before_filter :load_log_entry,
+                only: [:show, :edit, :update, :destroy]
+  before_filter :load_adventure_form_inputs,
+                only: [:new, :create, :edit, :update]
   before_filter :load_overrides, only: [:edit, :update]
 
-  before_filter(only: [:show]) { add_crumb @character.name, user_character_path(@character.user, @character) if @character }
-  before_filter(only: [:show]) { add_crumb @campaign.name, user_campaign_path(@campaign.user, @campaign) if @campaign }
+  before_filter(only: [:show]) do
+    add_crumb @character.name,
+              user_character_path(@character.user, @character) if @character
+  end
+  before_filter(only: [:show]) do
+    add_crumb @campaign.name,
+              user_campaign_path(@campaign.user, @campaign) if @campaign
+  end
 
-  before_filter(only: [:new, :create, :edit, :update]) { add_crumb @campaign.name, user_campaign_path(@campaign.user, @campaign) }
+  before_filter(only: [:new, :create, :edit, :update]) do
+    add_crumb @campaign.name,
+              user_campaign_path(@campaign.user, @campaign)
+  end
 
   before_filter(only: [:show]) { add_crumb @log_entry.adventure_title }
 
@@ -28,6 +40,7 @@ class CampaignLogEntriesController < AuthenticationController
     @log_entry   = @campaign.campaign_log_entries.new
     authorize @log_entry
 
+    @log_entry.characters = @campaign.characters
     @magic_items = [MagicItem.new]
     @magic_item_count = 0
   end
@@ -39,12 +52,17 @@ class CampaignLogEntriesController < AuthenticationController
     authorize @log_entry
 
     if @log_entry.save
-      redirect_to [@user, @campaign], flash: { notice: "Successfully created log entry #{@log_entry.adventure_title}" }
+      redirect_to [@user, @campaign],
+                  flash: { notice: 'Successfully created log entry'\
+                                   "#{@log_entry.adventure_title}" }
     else
       @magic_items      = @log_entry.magic_items
       @magic_item_count = @log_entry.magic_items.count
 
-      flash.now[:error] = "Failed to create log_entry #{@log_entry.adventure_title}: #{@log_entry.errors.full_messages.join(',')}"
+      error_message = 'Failed to create log_entry '\
+                      "#{@log_entry.adventure_title}: "\
+                      "#{@log_entry.errors.full_messages.join(',')}"
+      flash.now[:error] = error_message
       render :new
     end
   end
@@ -61,12 +79,16 @@ class CampaignLogEntriesController < AuthenticationController
     authorize @log_entry
 
     if @log_entry.update_attributes(log_entries_params)
-       redirect_to [@user, @campaign], flash: { notice: "Successfully updated log entry #{@log_entry.adventure_title}" }
+      redirect_to [@user, @campaign],
+                  flash: { notice: 'Successfully updated log entry'\
+                                   "#{@log_entry.adventure_title}" }
     else
       @magic_items      = @log_entry.magic_items
       @magic_item_count = @log_entry.magic_items.count
 
-      flash.now[:error] = "Failed to update log_entry #{@log_entry.adventure_title}: #{@log_entry.errors.full_messages.join(',')}"
+      flash.now[:error] = 'Failed to update log_entry'\
+                          "#{@log_entry.adventure_title}:"\
+                          "#{@log_entry.errors.full_messages.join(',')}"
       render :edit
     end
   end
@@ -75,7 +97,9 @@ class CampaignLogEntriesController < AuthenticationController
     authorize @log_entry
     @log_entry.destroy
 
-    redirect_to [@user, @campaign], flash: { notice: "Successfully deleted Campaign Log Entry #{@log_entry.id}" }
+    redirect_to [@user, @campaign],
+                flash: { notice: 'Successfully deleted Campaign Log Entry'\
+                                 "#{@log_entry.id}" }
   end
 
   protected
@@ -85,19 +109,23 @@ class CampaignLogEntriesController < AuthenticationController
   end
 
   def load_character
-    @character   = Character.find(params[:character_id]) if params[:character_id]
+    @character = Character.find(params[:character_id]) if params[:character_id]
   end
 
   def load_campaign
-    @campaign    = Campaign.find(params[:campaign_id]) if params[:campaign_id]
+    @campaign  = Campaign.find(params[:campaign_id]) if params[:campaign_id]
+  end
+
+  def log_entry
+    if @campaign
+      @campaign.campaign_log_entries.find(params[:id])
+    else
+      CampaignLogEntry.find(params[:id])
+    end
   end
 
   def load_log_entry
-    if @campaign
-      @log_entry = @campaign.campaign_log_entries.find(params[:id])
-    else
-      @log_entry = CampaignLogEntry.find(params[:id])
-    end
+    @log_entry ||= log_entry
   end
 
   def load_adventure_form_inputs
@@ -105,11 +133,18 @@ class CampaignLogEntriesController < AuthenticationController
   end
 
   def load_overrides
-    @use_adventure_override = !@adventure_form_inputs.find_by(name: @log_entry.adventure_title)
+    @use_adventure_override = !@adventure_form_inputs
+                              .find_by(name: @log_entry.adventure_title)
   end
 
   def log_entries_params
-    params.require(:campaign_log_entry).permit(:adventure_title, :session_num, :date_played, :xp_gained, :gp_gained, :renown_gained, :downtime_gained, :num_secret_missions, :location_played, :notes, character_ids: [], magic_items_attributes: [:id, :name, :rarity, :notes, :_destroy])
+    params.require(:campaign_log_entry)
+          .permit(:adventure_title, :session_num, :date_played,
+                  :xp_gained, :gp_gained, :renown_gained, :downtime_gained,
+                  :num_secret_missions, :location_played, :notes,
+                  character_ids: [],
+                  magic_items_attributes: [:id, :name, :rarity,
+                                           :notes, :_destroy])
   end
 
 end
