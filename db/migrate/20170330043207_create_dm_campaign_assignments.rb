b1a7b6a881ee60e80a6856acb11a384c0f5f99db
Ariel Thomas <Ariel.Thomas@gmail.com>
2017-03-31 23:34:56 -0500

    Migration fixes.

--- db/migrate/20170330043207_create_dm_campaign_assignments.rb
+++ db/migrate/20170330043207_create_dm_campaign_assignments.rb
@@ -1,14 +1,21 @@
 class CreateDmCampaignAssignments < ActiveRecord::Migration
-  def change
+  def up
     create_table :dm_campaign_assignments do |t|
       t.integer :user_id
       t.integer :campaign_id
     end
 
     Campaign.all.each do |campaign|
-      DmCampaignAssignment.create!(campaign: campaign, user: campaign.user)
+      next unless campaign && campaign.user_id
+      next unless User.find_by_id(campaign.user_id)
+      DmCampaignAssignment.create!(campaign_id: campaign.id, user_id: campaign.user_id)
     end
 
     remove_column :campaigns, :user_id, :integer
+  end
+
+  def down
+    add_column :campaigns, :user_id, :integer
+    drop_table :dm_campaign_assignments
   end
 end
--- db/migrate/20170330105802_add_character_id_to_magic_items.rb
+++ db/migrate/20170330105802_add_character_id_to_magic_items.rb
@@ -3,7 +3,7 @@ class AddCharacterIdToMagicItems < ActiveRecord::Migration
     add_column :magic_items, :character_id, :integer
 
     MagicItem.all.each do |mi|
-      mi.destroy and next unless mi.log_entry
+      mi.destroy and next unless mi.log_entry && mi.log_entry.character
       mi.update!(character_id: mi.log_entry.character.id)
     end
   end
