79874baf338ee3d0921821e74cdfb38a388a04ed
Ariel Thomas <Ariel.Thomas@gmail.com>
2017-04-06 22:01:15 -0500

    Add unique rarity option

--- app/models/magic_item.rb
+++ app/models/magic_item.rb
@@ -3,5 +3,5 @@ class MagicItem < ActiveRecord::Base
   belongs_to :trade_log_entry
   belongs_to :character
 
-  enum rarity: [:common, :uncommon, :rare, :very_rare, :legendary]
+  enum rarity: [:common, :uncommon, :rare, :very_rare, :legendary, :unique]
 end
