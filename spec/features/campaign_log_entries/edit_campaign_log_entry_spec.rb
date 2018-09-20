45bb73d7bb8245ad5fcd3fed79b99545ec7dbc7c
Zachery Moneypenny <whazzmaster@gmail.com>
2018-09-19 23:17:43 -0500

    Fixes edit_campaign_log_entry_specs

--- spec/features/campaign_log_entries/edit_campaign_log_entry_spec.rb
+++ spec/features/campaign_log_entries/edit_campaign_log_entry_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Campaign Log Entries', type: :feature do
+RSpec.feature 'Campaign Log Entries', type: :feature, js: true do
   before(:each) do
     @user = FactoryGirl.create(:user)
     login_as(@user, scope: :user)
@@ -25,9 +25,9 @@ RSpec.feature 'Campaign Log Entries', type: :feature do
     visit user_campaign_path(@user, @campaign)
 
     click_link 'Edit'
-    # find_link('Edit').trigger('click') # hack to fix previous line
 
     within('#campaign-log-entry-main-form') do
+      check 'Old Format', allow_label_click: true
       fill_in 'Adventure Title', with: 'Lost Mines of Phandelver'
 
       fill_in 'Session',            with: '22'
@@ -35,20 +35,23 @@ RSpec.feature 'Campaign Log Entries', type: :feature do
       fill_in 'Date Played',        with: '2016-08-01 19:00'
       fill_in 'XP Gained',          with: '1001'
       fill_in 'GP +/-',             with: '333'
-      fill_in 'Downtime&nbsp+/-',   with: '111'
+      fill_in 'Downtime',   with: '111'
       fill_in 'Renown',             with: '44'
       fill_in 'Mission',            with: '55'
+      check 'Manual Entry', allow_label_click: true
       fill_in 'Location',           with: 'Origins'
-      fill_in 'Notes',              with: 'Some Words'
+      # binding.pry
+      fill_in_editor_field "Some Words"
     end
 
-    check   @character4.name
-    uncheck @character5.name
+    check   @character4.name, allow_label_click: true
+    uncheck @character5.name, allow_label_click: true
 
     click_button 'Save'
 
     expect(CampaignLogEntry.count).to be(@campaign_log_entry)
 
+    check 'Old Format', allow_label_click: true
     expect(page).to have_text('2016-08-01 19:00')
     expect(page).to have_text('Lost Mines of Phandelver')
     expect(page).to have_text('22')
@@ -57,7 +60,6 @@ RSpec.feature 'Campaign Log Entries', type: :feature do
     expect(page).to have_text('111')
 
     click_link 'Show'
-    #find_link('Show').trigger('click') # hack to fix previous line
 
     expect(page).to have_text('Lost Mines of Phandelver')
     expect(page).to have_text('22')
