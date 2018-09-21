24e7af304cd270ec94f094825fa38940bc8fa958
Zachery Moneypenny <whazzmaster@gmail.com>
2018-09-21 07:52:20 -0500

    Fixes delete_campaign_log_entry_spec

--- spec/features/campaign_log_entries/delete_campaign_log_entry_spec.rb
+++ spec/features/campaign_log_entries/delete_campaign_log_entry_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Campaign Log Entries', type: :feature do
+RSpec.feature "Campaign Log Entries", type: :feature, js: true do
   before(:each) do
     @user = FactoryBot.create(:user)
     login_as(@user, scope: :user)
@@ -13,26 +13,28 @@ RSpec.feature 'Campaign Log Entries', type: :feature do
     @campaign_log_entry.characters = [@character1, @character2]
   end
 
-  context 'Delete a Campaign Log Entry' do
+  context "when deleted" do
     before(:each) do
       @campaign_log_entry_count = CampaignLogEntry.count
       visit user_campaign_path(@user, @campaign)
 
-      click_link 'Delete'
+      click_link "Delete", title: "Delete Campaign"
+      accept_confirm
     end
 
-    scenario 'Number of Campaign Log Entries decreases' do
+    scenario "the number of campaign log entries decreases" do
+      sleep 1 # Have to allow time for controller to process deletion
       expect(CampaignLogEntry.count).to be(@campaign_log_entry_count - 1)
     end
 
-    scenario 'Successfully deleted Campaign Log Entries' do
+    scenario "the flash is displayed with confirmation" do
       expect(page).to have_text("Successfully deleted Campaign Log Entry")
     end
 
-    scenario 'Does not show on campaign show page' do
+    scenario "the log no longer shows on the campaign page" do
       visit user_campaign_path(@campaign.user, @campaign)
 
-      expect(page).to_not have_text(@campaign_log_entry.date_played.strftime('%Y-%m-%d %H:%M'))
+      expect(page).to_not have_text(@campaign_log_entry.date_played.strftime("%Y-%m-%d %H:%M"))
       expect(page).to_not have_text(@campaign_log_entry.adventure_title)
       expect(page).to_not have_text(@campaign_log_entry.session_num)
       expect(page).to_not have_text(@campaign_log_entry.xp_gained)
@@ -40,10 +42,10 @@ RSpec.feature 'Campaign Log Entries', type: :feature do
       expect(page).to_not have_text(@campaign_log_entry.downtime_gained)
     end
 
-    scenario 'Does not show on character show page' do
+    scenario "the log does not show on the character page" do
       visit user_character_path(@character1.user, @character1)
 
-      expect(page).to_not have_text(@campaign_log_entry.date_played.strftime('%Y-%m-%d %H:%M'))
+      expect(page).to_not have_text(@campaign_log_entry.date_played.strftime("%Y-%m-%d %H:%M"))
       expect(page).to_not have_text(@campaign_log_entry.adventure_title)
       expect(page).to_not have_text(@campaign_log_entry.session_num)
       expect(page).to_not have_text(@campaign_log_entry.xp_gained)
