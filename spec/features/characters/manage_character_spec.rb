0f32225d3a797f9cf735ba9defb4d3490c89e1d9
Zachery Moneypenny <whazzmaster@gmail.com>
2018-09-22 10:49:52 -0500

    Fixes manage_character_spec

--- spec/features/characters/manage_character_spec.rb
+++ spec/features/characters/manage_character_spec.rb
@@ -1,17 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Characters page', type: :feature, js: true do
+RSpec.feature "Characters page", type: :feature, js: true do
   before(:each) do
     @user = FactoryBot.create(:user)
     login_as(@user, scope: :user)
   end
 
-  scenario 'Default to character page' do
+  scenario "Default to character page" do
     visit root_path
     expect(current_path).to eq(user_characters_path(@user))
   end
 
-  scenario 'Character page should have information' do
+  scenario "Character page should have information" do
     @character       = FactoryBot.create(:character, user: @user)
     @other_character = FactoryBot.create(:character, user: @user)
     visit user_characters_path(@user)
@@ -27,67 +27,66 @@ RSpec.feature 'Characters page', type: :feature, js: true do
     expect(page).to have_text(@other_character.faction.name)
   end
 
-  scenario 'Create a character' do
+  scenario "Create a character" do
     @lifestyle = FactoryBot.create :lifestyle
     @faction = FactoryBot.create :faction
     @character_count = Character.count
     visit user_characters_path(@user)
 
-    click_link 'New Character'
+    click_link "New Character"
 
-    fill_in 'Name *',             with: 'Rum'
-    fill_in 'Race',               with: 'Human (variant)'
-    fill_in 'Classes and Levels', with: 'Fighter 1'
-    fill_in 'Background',         with: 'Acolyte'
-    select  'Modest',             from: 'Lifestyle'
-    select  'Harpers',            from: 'Faction'
+    fill_in "Name *",             with: "Rum"
+    fill_in "Race",               with: "Human (variant)"
+    fill_in "Classes and Levels", with: "Fighter 1"
+    fill_in "Background",         with: "Acolyte"
+    select  "Modest",             from: "Lifestyle"
+    select  "Harpers",            from: "Faction"
 
-    click_button 'Save'
+    click_button "Save"
 
     expect(Character.count).to have_text(@character_count + 1)
-    expect(page).to have_text('Rum')
-    expect(page).to have_text('Human (variant)')
-    expect(page).to have_text('Fighter 1')
-    expect(page).to have_text('Harpers')
+    expect(page).to have_text("Rum")
+    expect(page).to have_text("Human (variant)")
+    expect(page).to have_text("Fighter 1")
+    expect(page).to have_text("Harpers")
   end
 
-  scenario 'Edit an existing character' do
+  scenario "Edit an existing character" do
     @character = FactoryBot.create(:character, user: @user)
-    @lifestyle     = FactoryBot.create :lifestyle,     name: 'Poor'
-    @faction       = FactoryBot.create :faction,       name: 'Zhentarim'
+    @lifestyle     = FactoryBot.create :lifestyle,     name: "Poor"
+    @faction       = FactoryBot.create :faction,       name: "Zhentarim"
     visit user_characters_path(@user)
 
-    # click_link "Edit"
-    find_link('Edit').trigger('click') # hack to fix previous line
+    click_link "Edit"
 
-    fill_in 'Name *',             with: 'Rum'
-    fill_in 'Race',               with: 'Human (variant)'
-    fill_in 'Classes and Levels', with: 'Fighter 1'
-    fill_in 'Background',         with: 'Criminal'
-    select  'Poor',               from: 'Lifestyle'
-    select  'Zhentarim',          from: 'Faction'
+    fill_in "Name *",             with: "Rum"
+    fill_in "Race",               with: "Human (variant)"
+    fill_in "Classes and Levels", with: "Fighter 1"
+    fill_in "Background",         with: "Criminal"
+    select  "Poor",               from: "Lifestyle"
+    select  "Zhentarim",          from: "Faction"
 
-    click_button 'Save'
+    click_button "Save"
+    click_link "Show"
 
-    # click_link "Show"
-    find_link('Show').trigger('click') # hack to fix previous line
-
-    expect(page).to have_text('Rum')
-    expect(page).to have_text('Human (variant)')
-    expect(page).to have_text('Fighter 1')
-    expect(page).to have_text('Criminal')
-    expect(page).to have_text('Poor')
-    expect(page).to have_text('Zhentarim')
-    expect(page).to have_text('rank 1')
+    expect(page).to have_text("Rum")
+    expect(page).to have_text("Human (variant)")
+    expect(page).to have_text("Fighter 1")
+    expect(page).to have_text("Criminal")
+    expect(page).to have_text("Poor")
+    expect(page).to have_text("Zhentarim")
+    expect(page).to have_text("rank 1")
   end
 
-  scenario 'Delete an existing character' do
+  scenario "Delete an existing character" do
     @character = FactoryBot.create(:character, user: @user)
     @character_count = Character.count
     visit user_characters_path(@user)
 
-    click_link 'Delete'
+    click_link "Delete"
+    accept_confirm
 
+    sleep 1 # Give controller time to process deletion
     expect(Character.count).to have_text(@character_count - 1)
   end
 end
