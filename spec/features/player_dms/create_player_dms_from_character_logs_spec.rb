05c7891c052a295ded881319682145f48c8a8817
Zachery Moneypenny <whazzmaster@gmail.com>
2018-09-23 22:40:56 -0500

    Fixes create_player_dms_from_character_logs_spec

--- spec/features/player_dms/create_player_dms_from_character_logs_spec.rb
+++ spec/features/player_dms/create_player_dms_from_character_logs_spec.rb
@@ -1,48 +1,53 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Character Log Entries', type: :feature do
+RSpec.feature "Character Log Entries", type: :feature, js: true do
   before(:each) do
     @user = FactoryBot.create(:user)
     login_as(@user, scope: :user)
-    @character = FactoryBot.create(:character, user: @user, name: 'Test Character')
-    @adventure = FactoryBot.create(:adventure, name: 'Lost Mines of Phandelver')
+    @character = FactoryBot.create(:character, user: @user, name: "Test Character")
+    @adventure = FactoryBot.create(:adventure, name: "Lost Mines of Phandelver")
   end
 
-  context 'When a user creates a Character Log Entry' do
+  context "When a user creates a Character Log Entry" do
     before(:each) do
       @player_dms_count = PlayerDm.count
 
       visit user_character_path(@user, @character)
-      all('a', text: 'Game Log').first.click
 
-      within('#character-log-entry-main-form') do
-        fill_in 'Adventure Title', with: 'Lost Mines of Phandelver'
+      click_button "New Entry"
+      click_link "Game Log"
 
-        fill_in 'Session',            with: '22'
-        fill_in 'Date Played',        with: '' #Hack for calendar popout
-        fill_in 'Date Played',        with: '2016-08-01 19:00'
-        fill_in 'XP Gained',          with: '1001'
-        fill_in 'GP +/-',             with: '333'
-        fill_in 'Downtime +/-',       with: '111'
-        fill_in 'Renown',             with: '44'
-        fill_in 'Mission',            with: '55'
-        fill_in 'Location',           with: 'Origins'
-        fill_in 'Notes',              with: 'Some Words'
+      within("#character-log-entry-main-form") do
+        # check "Old Format", allow_label_click: true
+        fill_in "Adventure Title", with: "Lost Mines of Phandelver"
 
-        fill_in 'DM Name',            with: 'Some DM'
-        fill_in 'DM DCI',             with: '66666666'
+        fill_in "Session",            with: "22"
+        fill_in "Date Played",        with: "" #Hack for calendar popout
+        fill_in "Date Played",        with: "2016-08-01 19:00"
+        fill_in "XP Gained",          with: "1001"
+        fill_in "GP +/-",             with: "333"
+        fill_in "Downtime +/-",       with: "111"
+        fill_in "Renown",             with: "44"
+        fill_in "Mission",            with: "55"
+        check "Manual Entry", allow_label_click: true, id: "use_location_override"
+        fill_in "Location",           with: "Origins"
+        fill_in_editor_field "Some Words"
+
+        check "Manual Entry", allow_label_click: true, id: "use_dm_override"
+        fill_in "DM Name",            with: "Some DM"
+        fill_in "DM DCI",             with: "66666666"
       end
 
-      click_button 'Save'
+      click_button "Save"
     end
 
-    it 'should create a player dm' do
+    it "should create a player dm" do
       expect(PlayerDm.count).to have_text(@player_dms_count + 1)
 
       visit user_player_dms_path(@user)
 
-      expect(page).to have_text('Some DM')
-      expect(page).to have_text('66666666')
+      expect(page).to have_text("Some DM")
+      expect(page).to have_text("66666666")
     end
   end
 end
