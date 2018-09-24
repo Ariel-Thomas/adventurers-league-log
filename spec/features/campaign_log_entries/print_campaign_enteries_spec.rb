03be72cfe3ff1fa66828b3eab9d1b0317a1d6180
Zachery Moneypenny <whazzmaster@gmail.com>
2018-09-23 22:09:58 -0500

    Fixes print_campaign_enteries_spec

--- spec/features/campaign_log_entries/print_campaign_enteries_spec.rb
+++ spec/features/campaign_log_entries/print_campaign_enteries_spec.rb
@@ -1,37 +1,38 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Campaign Log Entries', type: :feature do
+RSpec.feature "Campaign Log Entries", type: :feature, js: true do
   before(:each) do
     @user = FactoryBot.create(:user)
     login_as(@user, scope: :user)
     @character    = FactoryBot.create(:character, user: @user)
     @campaign_log_entry = FactoryBot.create(:campaign_log_entry)
     @campaign_log_entry.characters = [@character]
+
+    visit user_character_path(@user, @character)
+    click_button "Print"
   end
 
-  scenario 'Campaign Log Entry print full page should have information' do
-    visit user_character_path(@user, @character)
-    all('a', text: 'Print Full').first.click
+  scenario "Campaign Log Entry print full page should have information" do
+    click_link "Print Full"
+
+    expect(page).to have_text(@campaign_log_entry.adventure_title)
+    expect(page).to have_text(@campaign_log_entry.session_num)
+    expect(page).to have_text(@campaign_log_entry.xp_gained.to_i)
+    expect(page).to have_text(@campaign_log_entry.gp_gained.floor)
+    expect(page).to have_text(@campaign_log_entry.downtime_gained.to_i)
+    expect(page).to have_text(@campaign_log_entry.notes)
+    expect(page).to have_text(@campaign_log_entry.date_played.strftime("%Y-%m-%d %H:%M"))
+  end
+
+  scenario "Campaign Log Entry print condensed page should have information" do
+    click_link "Print Condensed"
 
     expect(page).to have_text(@campaign_log_entry.adventure_title)
     expect(page).to have_text(@campaign_log_entry.session_num)
     expect(page).to have_text(@campaign_log_entry.xp_gained)
     expect(page).to have_text(@campaign_log_entry.gp_gained.floor)
-    expect(page).to have_text(@campaign_log_entry.downtime_gained)
+    expect(page).to have_text(@campaign_log_entry.downtime_gained.to_i)
     expect(page).to have_text(@campaign_log_entry.notes)
-    expect(page).to have_text(@campaign_log_entry.date_played.strftime('%Y-%m-%d %H:%M'))
-  end
-
-  scenario 'Campaign Log Entry print condensed page should have information' do
-    visit user_character_path(@user, @character)
-    all('a', text: 'Print Condensed').first.click
-
-    expect(page).to have_text(@campaign_log_entry.adventure_title)
-    expect(page).to have_text(@campaign_log_entry.session_num)
-    expect(page).to have_text(@campaign_log_entry.xp_gained)
-    expect(page).to have_text(@campaign_log_entry.gp_gained.floor)
-    expect(page).to have_text(@campaign_log_entry.downtime_gained)
-    expect(page).to have_text(@campaign_log_entry.notes)
-    expect(page).to have_text(@campaign_log_entry.date_played.strftime('%Y-%m-%d %H:%M'))
+    expect(page).to have_text(@campaign_log_entry.date_played.strftime("%Y-%m-%d %H:%M"))
   end
 end
