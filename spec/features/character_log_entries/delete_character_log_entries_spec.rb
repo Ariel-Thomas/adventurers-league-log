d6859978fa34bdf62c4bf5fbde7be22a337dea0a
Zachery Moneypenny <whazzmaster@gmail.com>
2018-09-23 22:34:28 -0500

    Fixes delete_character_log_entries_spec

--- spec/features/character_log_entries/delete_character_log_entries_spec.rb
+++ spec/features/character_log_entries/delete_character_log_entries_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Character Log Entries', type: :feature do
+RSpec.feature "Character Log Entries", type: :feature, js: true do
   before(:each) do
     @user = FactoryBot.create(:user)
     login_as(@user, scope: :user)
@@ -9,22 +9,22 @@ RSpec.feature 'Character Log Entries', type: :feature do
     @character_log_entry.characters = [@character]
   end
 
-  scenario 'Delete a Character Log Entry' do
+  scenario "Delete a Character Log Entry" do
     @character_log_entry_count = CharacterLogEntry.count
     visit user_character_path(@user, @character)
 
-    click_link 'Delete'
-    # find_link('Delete').trigger('click') # hack to fix previous line
+    click_link "Delete"
+    accept_confirm
 
+    sleep 1
     expect(CharacterLogEntry.count).to be(@character_log_entry_count - 1)
 
     expect(page).to have_text("Successfully deleted " + @character_log_entry.adventure_title)
 
     visit user_character_path(@user, @character)
 
-    expect(page).to_not have_text(@character_log_entry.date_played.strftime('%Y-%m-%d %H:%M'))
+    expect(page).to_not have_text(@character_log_entry.date_played.strftime("%Y-%m-%d %H:%M"))
     expect(page).to_not have_text(@character_log_entry.adventure_title)
-    # expect(page).to_not have_text(@character_log_entry.session_num)
     expect(page).to_not have_text(@character_log_entry.xp_gained)
     expect(page).to_not have_text(@character_log_entry.gp_gained.floor)
     expect(page).to_not have_text(@character_log_entry.downtime_gained)
