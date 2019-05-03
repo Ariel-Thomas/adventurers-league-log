71f67854d21c5cf9387ca11e0b0a3d9fa34ca8f5
Chris C <devnuhl@gmail.com>
2019-05-03 15:15:11 -0500

    Route Change to Fix Reset Password URL
    When following the link in the email from sendgrid, it fails to load the app, because
    it's trying to hit adventurersleaguelog.com, instead of www.adventurersleaguelog.com,
    simply adding it to the address bar fixes the problem, and I believe this code change
    will fix it globally.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ AdventurersLeagueLog::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  Rails.application.routes.default_url_options[:host] = 'adventurersleaguelog.com'
+  Rails.application.routes.default_url_options[:host] = 'www.adventurersleaguelog.com'
 
   ActionMailer::Base.smtp_settings = {
     port: '587',
