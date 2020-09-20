743d27da612aff882e61966d570eaf730a657ef6
Ariel Thomas <Ariel.Thomas@gmail.com>
2020-09-19 22:27:08 -0500

    Add feedback for migration.

--- db/migrate/20200920003315_add_indexed_level_to_characters.rb
+++ db/migrate/20200920003315_add_indexed_level_to_characters.rb
@@ -2,6 +2,10 @@ class AddIndexedLevelToCharacters < ActiveRecord::Migration[5.2]
   def change
     add_column :characters, :indexed_level, :integer, default: 1
 
-    Character.in_batches.each_record {|c| c.update({})}
+    batch_count = Character.count / 100
+    Character.find_in_batches(batch_size: 100).with_index do |batch, batch_index|
+      puts "Processing #{batch_index} / #{batch_count}"
+      batch.each {|c| c.update({})}
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_003315) do
+ActiveRecord::Schema.define(version: 2020_09_19_214834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 2020_09_20_003315) do
     t.integer "automagic_downtime_toggle", default: 0
     t.string "season", default: ""
     t.string "tag", default: ""
-    t.integer "indexed_level", default: 1
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
